a589b9f719460231e383e293f9b3457603f10a64
BuildTools <ludvigljungberg2005@gmail.com>
2024-07-05 15:39:17 +0200

    Fix last issue

--- mod/TTT.Roles/RoleManager.cs
+++ mod/TTT.Roles/RoleManager.cs
@@ -83,8 +83,7 @@ public class RoleManager : PlayerHandler, IRoleService, IPluginBehavior
                 });
             
                 _roundService.GetLogsService().AddLog(new MiscAction("tased player " + targetRole.PlayerRole().FormatStringFullAfter(playerWhoWasDamaged.PlayerName), attacker));
-
-
+                
                 return HookResult.Stop;
             }
             
@@ -92,7 +91,7 @@ public class RoleManager : PlayerHandler, IRoleService, IPluginBehavior
             
             GetPlayer(playerWhoWasDamaged).SetDead(true);
 
-            if (info.Damage < playerWhoWasDamaged.Health) return HookResult.Continue;
+            if (info.Damage < playerWhoWasDamaged.PlayerPawn.Value.Health) return HookResult.Continue;
             
             info.Damage = 0;
             
